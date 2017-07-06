11c27cd1b82a6a0589d54e003dabd10b3a4cf87c
Viral <viral2100@gmail.com>
2017-07-06 14:10:20 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost","root","87654321","career");
+$conn = mysqli_connect("localhost","root","########","career");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 } 
